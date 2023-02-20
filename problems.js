b045dd191930d4167eb4c50266a67a9d6f5c9f2e
ÇETİN <cetinnnarasss@gmail.com>
2023-02-20 15:54:59 +0300

    prb-2

--- problems.js
+++ problems.js
@@ -9,4 +9,9 @@
 //! console.log(args.sort((a,b)=>a-b)[0])
 //! console.log(args.reduce((x, y) => x > y ? y : x ))
 
-2- 
+// 2-Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+//! function sum(numbers) {
+   //! return numbers.reduce((a, b) => a + b, 0); }
+  
+3-   
