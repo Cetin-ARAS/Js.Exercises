ff57be9e3437a57fabb6fe76cf365e61865167e7
ÇETİN <cetinnnarasss@gmail.com>
2023-02-20 15:22:41 +0300

    prb-1

---
+++ problems.js
@@ -0,0 +1,12 @@
+// 1-  Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+
+//! const args = [20, 50, -4]
+//! console.log(Math.min(...args))
+//! console.log(args.sort((a,b)=>a-b)[0])
+//! console.log(args.reduce((x, y) => x > y ? y : x ))
+
+2- 
