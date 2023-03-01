350b4439fd519c8ed18363fe46c4e9bcb7fe5f4f
ÇETİN <cetinnnarasss@gmail.com>
2023-03-01 23:59:15 +0300

    prb-10

--- problems.js
+++ problems.js
@@ -85,23 +85,38 @@
 //! }
 //! console.log(adet)
 
-//9-- aranan değerin yerini bulma
-const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-let x = 0;
-let arrLength = arr.length - 1
+//?9-- aranan değerin yerini bulma(binaary search)
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+// let x = 0;
+// let arrLength = arr.length - 1
 
-const binarySearch=(number) => {
-  while(x<arrLength){
-    const y = Math.floor(x + (arrLength - x) / 2)
-    if (arr[y] == number) {
-      return y
-    }
-    if ( arr[y] < number) {
-      x = y + 1
-    }else {
-      arrLength = y - 1
+// const binarySearch=(number) => {
+//   while(x<arrLength){
+//     const y = Math.floor(x + (arrLength - x) / 2)
+//     if (arr[y] == number) {
+//       return y
+//     }
+//     if ( arr[y] < number) {
+//       x = y + 1
+//     }else {
+//       arrLength = y - 1
+//     }
+//   }
+//   return -1
+// }
+// console.log(binarySearch(7))
+
+//? 10-- asal sayı bulma
+
+const number = 20
+let counter = 0
+
+const asalSayıBulma = (number) = {
+   for (let i = 2; i < number; i++) {
+    if ( number % i == 0 ) {
+      counter++;
+      break;
     }
   }
-  return -1
 }
-console.log(binarySearch(7))
+console.log(counter >0 ? "Asal Değil" : "Sayı asal")
